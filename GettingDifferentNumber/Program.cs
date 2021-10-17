70fa3d3d12b6bbcc9590ffef594ba4e338cab75a
Ronald Bett <rbett@jambopay.com>
2021-10-17 09:58:28 +0300

    Initial commit

---
+++ GettingDifferentNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GettingDifferentNumber
+{
+
+    class Solution
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] arr = new []{1,5,2,4};
+      
+            Console.WriteLine(GetDifferentNumber(arr));
+      
+        }
+  
+        // O(n^2)
+    }
+    
+}
